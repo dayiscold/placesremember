7275a9d55a729edaa10f58db335add10fe1495c3
dayiscold <asatulinv@yandex.ru>
2023-05-25 18:07:02 +0500

    update pip

--- backendplace/main/views.py
+++ backendplace/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'title': 'Главная страница'})
 
 def afterindex(request):
     return render(request, 'main/login.html')
